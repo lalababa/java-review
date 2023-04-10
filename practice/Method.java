6d35366fa0aa5f529d6a58b4aaad72e9ef73fbb1
lalababa <lalababadidi@gmail.com>
2023-04-10 12:13:20 -0400

    review switch and method knowledge

---
+++ practice/Method.java
@@ -0,0 +1,15 @@
+public class Method {
+  public static void main(String[] args) {
+    isEvenNumber();
+  }
+
+  public static void isEvenNumber(){
+    int num = 5;
+
+    if (num % 2 ==0 ){
+      System.out.println("even number");
+    } else {
+      System.out.println("odd number");
+    }
+  }
+}
